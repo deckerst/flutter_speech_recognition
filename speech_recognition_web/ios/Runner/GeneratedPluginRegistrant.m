3016ceca435229ffafc7691c00ad03882f190924
Thibault Deckers <thibault@bpuholdings.com>
2019-11-11 11:06:37 +0900

    packages upgrade

--- speech_recognition_web/ios/Runner/GeneratedPluginRegistrant.m
+++ speech_recognition_web/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,7 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<speech_recognition/SpeechRecognitionPlugin.h>)
 #import <speech_recognition/SpeechRecognitionPlugin.h>
+#else
+@import speech_recognition;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
